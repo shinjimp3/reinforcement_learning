895a235ebe08b98da498393b35d5aca5e198fbf4
shinjimp3 <tbw.rbcn.1211129@gmail.com>
2017-03-06 23:57:27 +0900

    Change list into numpy array and add moving image to RandomWalk2d

--- Environment.py
+++ Environment.py
@@ -1,13 +1,14 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 class RandomWalk:
-	def __init__(self, initial_state = [0]):
+	def __init__(self, initial_state = 0):
 		self.state = initial_state
 		self.initial_state = initial_state
-		self.steps = 0
-		self.state_size = [5]
-		self.action_size = [2]
+		self.step_cnt = 0
+		self.state_size = 5
+		self.action_size = 2
 
 
 	def step(self, action_index):
@@ -15,14 +16,14 @@ class RandomWalk:
 		reward = self.give_reward(self.state, action_index, next_state)
 		s = self.state
 		self.state = next_state
-		self.steps += 1
+		self.step_cnt += 1
 		return s, action_index, reward, next_state
 
 	def trans_state(self, state_index, action_index):
 		# P(s' | s, a) :define translate probably
-		action_set = [-1, 1]
-		state = state_index[0]
-		action = action_set[int(action_index[0])]
+		action_set = np.array([-1, 1])
+		state = state_index
+		action = action_set[action_index]
 
 		next_state = state + action
 		if random.random() < 0.1:
@@ -31,44 +32,43 @@ class RandomWalk:
 		if next_state < 0 or next_state > 4:
 			next_state = state
 		
-		return [next_state]
+		return next_state
 
 	def give_reward(self, state, action, next_state):
 		# R(r | s, a, s') :define reward function
-		if state == [4]:
+		if state == 4:
 			reward = 10
 		else:
 			reward = 0
 		return reward
 
 	def render(self):
-		view = str(self.steps) + ": " + "-"*self.state[0] + "o" + "-"*(4-self.state[0])
+		view = str(self.step_cnt) + ": " + "-"*self.state + "o" + "-"*(4-self.state)
 		print(view)
 
 	def reset(self):
 		self.state = self.initial_state
-		self.steps = 0
+		self.step_cnt = 0
 
 class RandomWalk2d(RandomWalk):
-	def __init__(self, initial_state = [0,0]):
+	def __init__(self, initial_state = np.array([0,0])):
 		self.state = initial_state
 		self.initial_state = initial_state
-		self.steps = 0
-		self.state_size = [5,5]
-		self.action_size = [3,3]
+		self.step_cnt = 0
+		self.state_size = np.array([5,5])
+		self.action_size = np.array([3,3])
 
 	def trans_state(self, state_index, action_index):
 		# P(s' | s, a) :define translate probably
-		state = np.array(state_index)
-		action = np.array(action_index)
-		action = action-1
+		state = state_index
+		action = np.array(action_index) - 1
 
 		next_state = state + action
 		if random.random() < 0.1:
 			next_state = state - action
 		next_state = np.clip(next_state,0,4)
 		
-		return list(next_state)
+		return next_state
 
 	def give_reward(self, state, action, next_state):
 		# R(r | s, a, s') :define reward function
@@ -79,10 +79,20 @@ class RandomWalk2d(RandomWalk):
 		return reward
 
 	def render(self):
-		print(self.steps)
+		mat = np.zeros((5,5))
+		mat[4,:] = np.ones((1,5))
+		mat[self.state[0], self.state[1]] = 0.5
+		plt.imshow(mat, interpolation='none', cmap='brg')
+		plt.pause(.001)
+		"""print(self.step_cnt)
 		for i in range(5):
 			if i == self.state[0]:
 				view = "+"*self.state[1] + "o" + "+"*(4-self.state[1])
 			else:
 				view = "+"*5
-			print(view)
+			if i == 0:
+				sys.stdout.write("\r%s\n" % view)
+			else:
+				sys.stdout.write("%s\n" % view)
+		sys.stdout.flush()
+		"""
--- QlearningAgent.py
+++ QlearningAgent.py
@@ -4,10 +4,10 @@ import random
 class QlearningAgent:
 	def __init__(self, state_size, action_size):
 		#self.Qtable = np.zeros(state_size+action_size)
-		self.state = [0]
 		self.qtable = Qtable(state_size, action_size)
 		self.action_size = action_size
 		self.state_size = state_size
+		self.state = np.zeros(state_size)
 
 	def observe_state(self, new_state):
 		self.state = new_state
@@ -18,9 +18,10 @@ class QlearningAgent:
 		if random.random() > epsilon or not train:
 			action = self.qtable.argmax(state)
 		else:
-			action = []
+			action = np.array([])
 			for a_s in self.action_size:
-				action.append(np.random.randint(a_s))
+				#action.append(np.random.randint(a_s))
+				action = np.append(action, np.array(np.random.randint(a_s)))
 		#print(action)
 		return action
 
@@ -47,44 +48,45 @@ class Qtable:
 		return self.straight_Q[index]
 
 	def max(self, state):
-		start_i = self.get_sa_index(state, [0]*len(self.action_size))
+		start_i = self.get_sa_index(state, np.zeros(self.action_size))
 		end_i =  start_i + np.prod(self.action_size)
 		return np.max(self.straight_Q[start_i:end_i])
 
 	def argmax(self, state):
-		start_i = self.get_sa_index(state, [0]*len(self.action_size))
+		start_i = self.get_sa_index(state, np.zeros(self.action_size))
 		end_i =  start_i + np.prod(self.action_size)
 		index = np.argmax(self.straight_Q[start_i:end_i])
-		action_i = []
-		size_list = self.action_size
-		for i in range(len(size_list)):
-			action_i.append(index%size_list[-i-1])
-			index = (index-index%size_list[-i-1])/size_list[-i-1]
-		action_i.reverse()
+		action_i = np.array([])
+		size_array = self.action_size
+		for i in range(size_array.size):
+			action_i = np.append(action_i, index%size_array[-i-1])
+			index = (index-index%size_array[-i-1])/size_array[-i-1]
+		action_i = np.flipud(action_i)
 		return action_i
 
 	def get_sa_index(self, state, action):
-		index_list = state+action
-		size_list = self.state_size + self.action_size
+		index_array = np.append(state, action) 
+		size_array = np.append(self.state_size, self.action_size)
 		index = 0
-		for sa_index in range(len(index_list)):
-			if sa_index < len(index_list)-1:
-				index += index_list[sa_index]*np.prod(size_list[sa_index+1:])
+		for sa_index in range(index_array.size):
+			if sa_index < index_array.size-1:
+				index += index_array[sa_index]*np.prod(size_array[sa_index+1:])
 			else:
-				index += index_list[sa_index]
+				index += index_array[sa_index]
 		return index
 
 	def show(self):
-		self.recurent_show([0],self.state_size+self.action_size)
+		np.set_printoptions(formatter={'float': '{: 0.3f}'.format})
+		self.recurent_show(np.zeros(1),np.append(self.state_size, self.action_size))
 
-	def recurent_show(self, index_list, size_list):
-		dim = len(index_list)
-		full_dim = len(size_list)
+	def recurent_show(self, index_array, size_array):
+		dim = index_array.size
+		full_dim = size_array.size
 		if dim == full_dim:
-			start_i = self.get_sa_index(index_list[:len(self.state_size)], index_list[len(self.state_size):])
-			end_i = start_i + size_list[-1]
+			start_i = self.get_sa_index(index_array[:self.state_size.size], index_array[self.state_size.size:])
+			end_i = start_i + size_array[-1]
 			print(self.straight_Q[start_i:end_i])
 		else:
-			for index in range(size_list[dim-1]):
-				self.recurent_show(index_list+[index], size_list)
+			for index in range(size_array[dim-1]):
+				self.recurent_show(np.append(index_array, [index]), size_array)
 			print("")
